40b97472068cd9a1c40513b957d630dd7a85da45
ammumike <saifiamaan445@gmail.com>
2021-09-17 01:36:29 +0530

    Tetris shapes added

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,9 +1,7 @@
 import React from "react";
 
-const Cell = ({type})=>{
-    <div>
-        cell
-    </div>
-}
+const Cell = ({type})=>(
+    <div>cell</div>
+)
 
 export default Cell;
--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { createStage } from "../gameHelper";
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton"
@@ -7,7 +8,7 @@ import StartButton from "./StartButton"
 const Tetris = ()=>{
     return(
         <div>
-            <Stage/>
+            <Stage stage={createStage()} />
             <aside>
                 <div>
                     <Display text="Score"/>
