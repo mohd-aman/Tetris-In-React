107e344f4e6e29fd18ad4239beb1651686229464
ammumike <saifiamaan445@gmail.com>
2021-09-17 17:50:15 +0530

    props passed to Stage components

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -7,17 +7,31 @@ import { StyledTetrisWrapper,StyledTetris } from "./styles/StyledTetris";
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton"
+import { useState } from "react/cjs/react.development";
+import { usePlayer } from "../hooks/usePlayer";
 
 const Tetris = ()=>{
+    const [dropTime,setDropTime] = useState(null);
+    const [gameOver,setGameOver] = useState(false);
+
+    const [player] = usePlayer();
+    const [stage,setStage] = useStage(player);
+
+
+
     return(
         <StyledTetrisWrapper>
             <StyledTetris>
             <Stage stage={createStage()} />
             <aside>
+                {gameOver?(
+                    <Display gameOver={gameOver} text="Game Over"/>
+                ):(<div>
                     <Display text="Score"/>
                     <Display text = "Rows"/>
                     <Display text="Level"/>
-              
+                    </div>
+                )}
                 <StartButton/>
             </aside>
             </StyledTetris>
