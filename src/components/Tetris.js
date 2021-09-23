37284e846f1694960bb0531627c5e2f4bf8a70c1
ammumike <saifiamaan445@gmail.com>
2021-09-23 16:45:41 +0530

    custom hooks updated

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -15,15 +15,47 @@ const Tetris = ()=>{
     const [dropTime,setDropTime] = useState(null);
     const [gameOver,setGameOver] = useState(false);
 
-    const [player] = usePlayer();
+    const [player,updatePlayerPos,resetPlayer] = usePlayer();
     const [stage,setStage] = useStage(player);
 
+    console.log('re-render');
 
+    const movePlayer = dir=>{
+        updatePlayerPos({x:dir,y:0});
+    }
+
+    const startGame = () =>{
+        console.log("test");
+        setStage(createStage());
+        resetPlayer();
+    }   
+
+    const drop = () => {
+        updatePlayerPos({ x: 0, y: 1, collided: false })
+      }
+
+    const dropPlayer = ()=>{
+        drop();
+    }
+
+    const move = ({keyCode}) => {
+        if(!gameOver){
+            if(keyCode === 37){
+                movePlayer(-1);
+            }
+            else if(keyCode === 39){
+                movePlayer(1);
+            }
+            else if(keyCode === 40){
+                dropPlayer();
+            }
+        }
+    }
 
     return(
-        <StyledTetrisWrapper>
+        <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e=>move(e)}>
             <StyledTetris>
-            <Stage stage={createStage()} />
+            <Stage stage={stage} />
             <aside>
                 {gameOver?(
                     <Display gameOver={gameOver} text="Game Over"/>
@@ -33,7 +65,7 @@ const Tetris = ()=>{
                     <Display text="Level"/>
                     </div>
                 )}
-                <StartButton/>
+                <StartButton callback={startGame}/>
             </aside>
             </StyledTetris>
         </StyledTetrisWrapper>
