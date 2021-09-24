a4b00954f588a5f0eb4d620f7b5ce46de03cd8ce
ammumike <saifiamaan445@gmail.com>
2021-09-24 15:59:58 +0530

    merge tetris

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -3,7 +3,22 @@ import { createStage } from "../gameHelper";
 
 export const useStage = (player,resetPlayer) =>{
     const [stage,setStage] = useState(createStage());
+    const [rowsCleared,setRowsCleard] = useState(0);
+
     useEffect(()=>{
+        setRowsCleard(0);
+
+        const sweepRows = newStage=>
+        newStage.reduce((ack,row)=>{
+            if(row.findIndex(cell=>cell[0] === 0) === -1){
+                setRowsCleard(prev=>prev+1);
+                ack.unshift(new Array(newStage[0].length).fill([0,'clear']));
+                return ack;
+            }
+            ack.push(row);
+            return ack;
+        },[])
+
         const updateStage = prevStage=>{
             const newStage = prevStage.map(row=>
                 row.map(cell=>(cell[1] === 'clear'?[0,'clear']:cell)),
@@ -21,10 +36,11 @@ export const useStage = (player,resetPlayer) =>{
                 });
                 if(player.collided){
                     resetPlayer();
+                    return sweepRows(newStage);
                 }
                 return newStage;
         };
         setStage(prev=>updateStage(prev));
     },[player,resetPlayer]);
-    return [stage,setStage];
+    return [stage,setStage,rowsCleared];
 }
